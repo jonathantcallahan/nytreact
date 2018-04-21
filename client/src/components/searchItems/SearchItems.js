fa38ff1030961fab177d72cf28d3482d7d79e0d2
unknown <jtc7537@gmail.com>
2018-04-21 01:21:53 -0400

    work on db

---
+++ client/src/components/searchItems/SearchItems.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class SearchItems extends React.Component {
+
+    render(){
+        return(
+            <div id={this.props.key}>
+                <a href={this.props.web_url}>
+                    <h3>{this.props.snippet}</h3>
+                </a>
+                <button 
+                    onClick={() => this.props.save(this.props.snippet, this.props.web_url)}>
+                    Save to favorites
+                </button>
+            </div>
+        )
+    }
+}
+    
+
+export default SearchItems
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import Search from './../components/search/Search'
 import React from 'react'
 import axios from 'axios'
+import SearchItems from './../components/searchItems/SearchItems'
 
 class Home extends React.Component {
     constructor(){
@@ -24,15 +25,32 @@ class Home extends React.Component {
             .get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${this.state.topic}&api-key=e80d2a443a884de39f5a65f217ff0f12`)
             .then(data => {
                 console.log(data.data.response.docs)
-                this.setState({ articles:data.data.response.docs})
+                this.setState({ articles:data.data.response.docs })
             })
             .catch(err => console.log(err))
       }
 
+      saveArticle = (title, href) => {
+        axios
+            .post('http://localhost:3001/', {
+                title: title,
+                date: '',
+                href: href
+            })
+            .then(res => console.log(res))
+      }
+
       render(){
           return(
-              <Search set={this.setSearch} update={this.renderArticles} state={this.state}/>
-          )
+            <div>
+                <Search set={this.setSearch} update={this.renderArticles} state={this.state}/>
+                {
+                    this.state.articles.map(e => {
+                       return <SearchItems save={this.saveArticle} snippet={e.snippet} key={e._id} web_url={e.web_url}/>
+                    })
+                }
+            </div>  
+            )
       }
 }
 
