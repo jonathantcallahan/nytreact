fb5db855988ac1e8ca44d85f6b6cc79caaa8f919
unknown <jtc7537@gmail.com>
2018-04-28 01:01:11 -0400

    deployment

--- server.js
+++ server.js
@@ -9,11 +9,15 @@ const PORT = process.env.PORT || 3001
 app.use(bodyParser.urlencoded({ extended:true }))
 app.use(bodyParser.json())
 
-//if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, 'client/build')))
-//}
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'))
+}
 
 app.use('/api/article',routes)
+
+app.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
+})
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nytreact')
 
