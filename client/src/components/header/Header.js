ee5fa66b12f9f6a310f2010fb03093e05674d8a5
unknown <jtc7537@gmail.com>
2018-04-28 01:42:23 -0400

    styling

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Button from 'material-ui/Button'
+import './Header.css'
 
 const Header = () => (
     <div>
-        <h1>New York Times Article Scrubber</h1>
-        <h3>Search for and archive articles!</h3>
-        <Link to='/favorites'>
-            <Button variant='raised' color='primary'>favorites</Button>
-        </Link>
+        <div className='title-cont'>
+            <h1>New York Times Article Scrubber</h1>
+            <h3>Search for and archive articles!</h3>
+        </div>
+        <div className='btn-container'>
+            <Link to='/favorites' className='btn-link'>
+                <Button variant='raised' color='primary'>favorites</Button>
+            </Link>
+            
+            <Link to='/' className='btn-link'>
+                <Button variant='raised' color='default'>Home</Button>
+            </Link>
+        </div>
         
-        <Link to='/'>
-            <Button variant='raised' color='default'>Home</Button>
-        </Link>
     </div>
         
 )
--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Button from 'material-ui/Button'
-
+import './search.css'
 
 
 class Search extends React.Component {
@@ -8,15 +8,19 @@ class Search extends React.Component {
     render(){
         return(
             <div>
-                <div>
+                <div className='search-cont'>
+                    <label htmlFor='term'>Search Term</label>
                     <input 
+                        className='in'
+                        id='term'
                         type='text' 
                         name='topic' 
                         value={this.props.state.topic}
-                        onChange={this.props.set}
-                        placeholder='Search Term'/>
+                        onChange={this.props.set}/>
+                    <br />
                     <label htmlFor='start'>Start Date</label>    
                     <input 
+                        className='in'
                         id='start'
                         type='text'
                         name='start'
@@ -24,15 +28,18 @@ class Search extends React.Component {
                         value={this.props.state.start}
                         onChange={this.props.set}
                         />
+                    <br />    
                     <label htmlFor='end'>End Date</label>
                     <input 
+                        className='in'
                         id='end'
                         type='text'
                         name='end'
                         placeholder='YYYYMMDD'
                         value={this.props.state.end}
                         onChange={this.props.set}/>
-                    <Button variant='raised' color='primary' onClick={this.props.update}>Search</Button>
+                    <br />    
+                    <Button className='btn' variant='raised' color='primary' onClick={this.props.update}>Search</Button>
 
                 </div>    
             </div>
